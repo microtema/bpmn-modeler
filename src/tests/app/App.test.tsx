dfc60a927a237518d4e373757a4757cdbb941fb1
mtema <mario.tema@abas.de>
2018-12-02 12:17:20 +0100

    First Draft

---
+++ src/tests/app/App.test.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from "../../app/App";
+
+describe('App', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App/>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('render correctly text component', () => {
+        expect({foo: 'bar'}).toMatchSnapshot();
+    });
+});
