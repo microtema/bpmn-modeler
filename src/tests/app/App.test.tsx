5a5cffb0ba2d22a663e9d9033f28c878af5d4561
mtema <mario.tema@abas.de>
2018-12-02 13:16:00 +0100

    Support format

--- src/tests/app/App.test.tsx
+++ src/tests/app/App.test.tsx
@@ -3,13 +3,13 @@ import * as ReactDOM from 'react-dom';
 import App from '../../app/App';
 
 describe('App', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 
-  it('render correctly text component', () => {
-    expect({ foo: 'bar' }).toMatchSnapshot();
-  });
+    it('render correctly text component', () => {
+        expect({ foo: 'bar' }).toMatchSnapshot();
+    });
 });
